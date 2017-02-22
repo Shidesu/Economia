d99bb54c13c2e363f44843053aa24758a0826851
ALEXANDRE-PC\Alexandre <anchisialexandre@wanadoo.fr>
2017-02-22 19:17:49 +0100

    Modifications sur economia.java --> Ajout des fonctions onEnable() et onDisable() qui sont appelée respactivement lors de l'activation du plugin et de sa désactivation. Modification de plugin.yml -> Des erreurs étaient présentes ce qui empêchait le "plugin" d'être démarré par le serveur. Modification de pom.xml -> Rajout d'une dépendance pour l'API yaml.

--- src/main/java/io/github/shidesu/economia/economia.java
+++ src/main/java/io/github/shidesu/economia/economia.java
@@ -6,4 +6,16 @@ import org.bukkit.plugin.java.JavaPlugin;
  * Created by Alexandre on 22/02/2017.
  */
 public class economia extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+        getLogger().info("Economia chargée !");
+    }
+
+    @Override
+    public void onDisable(){
+        getLogger().info("Economia désactivée !");
+    }
+
+
 }
