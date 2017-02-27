959a57c13865c5c96030e76aa2ad0a58b301ea40
Kao <anchisialexandre@wanadoo.fr>
2017-02-27 21:46:43 +0100

    Correction d'erreurs dans ces deux fichiers.

--- src/main/java/io/github/shidesu/economia/managers/CommandsManager.java
+++ src/main/java/io/github/shidesu/economia/managers/CommandsManager.java
@@ -20,11 +20,12 @@ public class CommandsManager implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
-        if(command.getName().equalsIgnoreCase("ecopaid")){
+        if(command.getName().equalsIgnoreCase("ecopaid") && args.length >= 1 && args[0].equals("Test")){
+            Bukkit.broadcastMessage("Vous avez utilisé la commande de test ://");
+            return true;
+        }else if(command.getName().equalsIgnoreCase("ecopaid")){
             Bukkit.broadcastMessage("Vous avez payé le vent");
             return true;
-        }else if(command.getName().equalsIgnoreCase("ecopaid") && args[0].equals("Test")){
-            Bukkit.broadcastMessage("Vous avez utilisé la commande de test ://");
         }
 
         return false;
